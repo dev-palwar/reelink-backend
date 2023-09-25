f4f02de112ad84d127b878be0fb245f496fff9dc
dev <devpalwar06@gmail.com>
2023-09-25 14:55:37 +0530

    Added condition callback uri in google strategy

--- google.js
+++ google.js
@@ -4,18 +4,24 @@ const { Strategy: GoogleStrategy } = require("passport-google-oauth20");
 const passport = require("passport");
 const User = require("./Model/users.js");
 
+// Determine the redirect URI based on the environment
+const isProduction = process.env.NODE_ENV === 'production';
+const redirectURI = isProduction
+  ? 'https://reelink.onrender.com/authRoute/callBack'
+  : 'http://localhost:8000/authRoute/callBack';
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "/authRoute/callBack",
+      callbackURL: redirectURI, // Use the conditional redirect URI
       scope: ["profile", "email"],
     },
     async function (accessToken, refreshToken, profile, cb) {
       const resFromDB = await User.findOne({ email: profile._json.email });
       if (resFromDB) {
-        return cb(null, resFromDB); 
+        return cb(null, resFromDB);
       } else {
         const newUser = await User.create({
           googleId: profile.id,
@@ -23,7 +29,7 @@ passport.use(
           picture: profile._json.picture,
           email: profile._json.email,
         });
-        return cb(null, newUser); 
+        return cb(null, newUser);
       }
     }
   )
--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://reelink.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
